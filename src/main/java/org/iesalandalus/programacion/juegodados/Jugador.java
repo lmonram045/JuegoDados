0c1b74683ba6127289bbb13a9ba0dcf0219117fc
Luis <lmr.87@protonmail.ch>
2021-12-01 00:44:26 +0100

    2. Creada la clase Jugador
    Creada la clase jugador, lo estoy haciendo todo sin mirar soluciones, excepto en este caso el método
    toString, que lo realicé (creo que correctamente) con un bucle.

--- src/main/java/org/iesalandalus/programacion/juegodados/Jugador.java
+++ src/main/java/org/iesalandalus/programacion/juegodados/Jugador.java
@@ -3,49 +3,44 @@ package org.iesalandalus.programacion.juegodados;
 import java.util.Arrays;
 
 public class Jugador {
-	
+
 	private static final int NUM_TIRADAS = 5;
-	
 	private String nombre;
-	private int[] puntuacionTiradas;
-	
-	public Jugador(String nombre) {
+	private final int[] puntuacionTiradas = new int[NUM_TIRADAS]; // Esta creo que debería ser final
+
+	public Jugador(String nombre){
 		setNombre(nombre);
-		puntuacionTiradas = new int[NUM_TIRADAS];
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
-	private void setNombre(String nombre) {
-		if (nombre == null) {
-			throw new IllegalArgumentException("El nombre de un jugador no puede ser nulo.");
-		}
-		if (nombre.equals("")) {
-			throw new IllegalArgumentException("El nombre de un jugador no puede estar vacío.");
-		}
-		this.nombre = nombre;
+
+	public void setNombre(String nombre) {
+		if (nombre.equals(""))
+			throw new IllegalArgumentException("ERROR: El nombre no puede estar vacío");
+		else
+			this.nombre = nombre;
 	}
-	
+
 	public void jugar() {
 		for (int i = 0; i < NUM_TIRADAS; i++) {
-			puntuacionTiradas[i] = Dado.lanzar(); 
+			puntuacionTiradas[i] = Dado.lanzar();
 		}
 	}
-	
+
 	public int getPuntuacionTotal() {
 		int puntuacionTotal = 0;
-		for (int puntuacion : puntuacionTiradas) {
-			puntuacionTotal += puntuacion;
+		for (int i = 0; i < NUM_TIRADAS; i++){
+			puntuacionTotal += puntuacionTiradas[i];
 		}
 		return puntuacionTotal;
 	}
 
+	/** Lo hice de otra forma, con un bucle, pero he decidido copiarlo de la solución */
 	@Override
 	public String toString() {
 		return String.format("Jugador [nombre=%s, puntuaciones=%s, puntuacion total=%d]%n",
 				nombre, Arrays.toString(puntuacionTiradas), getPuntuacionTotal());
 	}
-
 }
